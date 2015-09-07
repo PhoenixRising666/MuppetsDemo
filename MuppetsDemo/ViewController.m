f7429211126cc54569b5d8092cd2f75c4c477fd2
Morgan Lemke <morganl2010@gmail.com>
2015-09-07 17:05:12 -0400

    Initial Commit

---
+++ MuppetsDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MuppetsDemo
+//
+//  Created by Morgan Lemke on 9/7/15.
+//  Copyright (c) 2015 Morgan Lemke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MuppetsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MuppetsDemo
+//
+//  Created by Morgan Lemke on 9/7/15.
+//  Copyright (c) 2015 Morgan Lemke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
